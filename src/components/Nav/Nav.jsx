bc5120bcfcbde06f62f2ef4d885a133acedf23f7
vzarate26 <vzarate26@gmail.com>
2022-02-26 17:33:11 -0500

    flesh out navbar css

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Nav.css'
 
 const Nav = (props) => {
   return (
-    <nav>
+    <nav className='navbar'>
       <Link to="/signup">Sign UP</Link>
     </nav>
   )
