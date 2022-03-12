32e4ea6b01efb79adf7f2b7b5de9aa07a6228835
vzarate26 <vzarate26@gmail.com>
2022-03-11 19:13:34 -0500

    add proxy local host to server

--- server.js
+++ server.js
@@ -34,8 +34,8 @@ app.get('/*', (req, res) => {
     )
 })
 
-const PORT = process.env.PORT || 3001
+const port = process.env.PORT || 3001
 
-app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`)
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`)
 })
